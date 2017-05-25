e548750323f312273458aaf7d11adb4aa33012e4
Marat Habibullin <maratx387@gmail.com>
2017-05-25 10:53:38 +0300

    JsonMutable template is split into .h and .tpp

--- src/json_cpp/internal/proxy_impl/json_mutable.h
+++ src/json_cpp/internal/proxy_impl/json_mutable.h
@@ -28,67 +28,20 @@ public:
 
     using JsonConst<CP>::operator[];
 
-    P operator[](std::string const &field_name) {
-        auto &r = const_cast<JsonValuePtr &>(static_cast<JsonMutable<P, CP> const &>(*this).AccessField(field_name));
-        return ProxyJsonRef(r);
-    }
-
-    P operator[](ArraySizeType index) {
-        auto &r = const_cast<JsonValuePtr &>(static_cast<JsonMutable<P, CP> const &>(*this).AccessElem(index));
-        return ProxyJsonRef(r);
-    }
+    P operator[](std::string const &field_name);
+    P operator[](ArraySizeType index);
 
     using JsonConst<CP>::ObjectBegin;
     using JsonConst<CP>::ObjectEnd;
+    
     using JsonConst<CP>::ArrayBegin;
     using JsonConst<CP>::ArrayEnd;
 
-    ObjectIterator ObjectBegin() {
-        return this->template ApplyToValueAs<repr::JsonObject>(
-            "get object iterator",
-            [this](std::shared_ptr<repr::JsonObject> const &o) {
-                return utils::MakeMappingBegin(
-                    o->value().begin(),
-                    std::bind(&JsonMutable<P, CP>::ProxyPair, this, std::placeholders::_1)
-                );
-            }
-        );  
-    }
-    ObjectIterator ObjectEnd() {
-        return this->template ApplyToValueAs<repr::JsonObject>(
-            "get object iterator",
-            [this](std::shared_ptr<repr::JsonObject> const &o) {
-                return utils::MakeMappingEnd(
-                    o->value().end(),
-                    std::bind(&JsonMutable<P, CP>::ProxyPair, this, std::placeholders::_1)
-                );
-            }
-        );
-    }
+    ObjectIterator ObjectBegin();
+    ObjectIterator ObjectEnd();
 
-    ArrayIterator ArrayBegin() {
-        return this->template ApplyToValueAs<repr::JsonArray>(
-            "get array iterator",
-            [this](std::shared_ptr<repr::JsonArray> const &a) {
-                return utils::MakeMappingBegin(
-                    a->value().begin(),
-                    std::bind(&JsonMutable<P, CP>::ProxyJsonRef, this, std::placeholders::_1)
-                );
-            }
-        );
-    }
-
-    ArrayIterator ArrayEnd() {
-        return this->template ApplyToValueAs<repr::JsonArray>(
-            "get array iterator",
-            [this](std::shared_ptr<repr::JsonArray> const &a) {
-                return utils::MakeMappingEnd(
-                    a->value().end(),
-                    std::bind(&JsonMutable<P, CP>::ProxyJsonRef, this, std::placeholders::_1)
-                );
-            }
-        );
-    }
+    ArrayIterator ArrayBegin();
+    ArrayIterator ArrayEnd();
 protected:
     using typename JsonConst<CP>::JsonValuePtr;
 
@@ -100,14 +53,11 @@ protected:
         return std::make_pair(p.first, ProxyJsonRef(p.second));
     }
 
-    void AddElemToArray(JsonValuePtr const &elem) {
-        return this->template ApplyToValueAs<repr::JsonArray>(
-            "append element to json array",
-            [&](std::shared_ptr<repr::JsonArray> const &a) { a->value().push_back(CopyJsonTree(elem)); }
-        );
-    }
+    void AddElemToArray(JsonValuePtr const &elem);
 };
 
 }}}
+
+#include "json_mutable.tpp"
 
 #endif
